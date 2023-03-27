cad30860eee70fb2ce862e9ed8283d732c185f7a
annafrymark <anna.frymark@gmail.com>
2023-03-27 17:19:46 +0200

    files from 02-phonebook

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,64 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React, { Component } from 'react';
+import ContactForm from './ContactForm/ContactForm';
+import ContactList from './ContactList/ContactList';
+import Filter from './Filter/Filter';
+import css from './app.module.css';
+
+class App extends Component {
+  state = {
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
+    filter: '',
+  };
+
+  addNewContact = newContact => {
+    this.setState({
+      contacts: [...this.state.contacts, newContact],
+    });
+    console.log(newContact);
+  };
+
+  deleteContact = id => {
+    this.setState({
+      contacts: this.state.contacts.filter(contact => contact.id !== id),
+    });
+  };
+
+  onFilterChange = newFilter => {
+    this.setState({
+      filter: newFilter,
+    });
+  };
+
+  filterContacts = () => {
+    return this.state.contacts.filter(contact =>
+      contact.name.toLowerCase().includes(this.state.filter.toLowerCase())
+    );
+  };
+
+  render() {
+    const filterValue = this.state.filter;
+
+    return (
+      <div className={css.container}>
+        <h1>Phonebook</h1>
+        <ContactForm
+          contacts={this.state.contacts}
+          addNewContact={this.addNewContact}
+        />
+        <h2>Contacts</h2>
+        <Filter filterValue={filterValue} onChange={this.onFilterChange} />
+        <ContactList
+          contacts={this.filterContacts()}
+          deleteContact={this.deleteContact}
+        />
+      </div>
+    );
+  }
+}
+
+export default App;
