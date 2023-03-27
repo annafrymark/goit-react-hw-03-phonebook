9a64078052b99b3cac148a888dcbb3d6846c73f5
annafrymark <anna.frymark@gmail.com>
2023-03-28 00:46:18 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,14 +6,27 @@ import css from './app.module.css';
 
 class App extends Component {
   state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
+    contacts: [],
     filter: '',
   };
+
+  componentDidMount = () => {
+    if (localStorage.getItem('contacts') != null) {
+      try {
+        this.setState({
+          contacts: JSON.parse(localStorage.getItem('contacts')),
+        });
+      } catch (e) {
+        console.error('Wrong JSON format of contacts');
+      }
+    }
+  };
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.contacts.length !== this.state.contacts.length) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
 
   addNewContact = newContact => {
     this.setState({
